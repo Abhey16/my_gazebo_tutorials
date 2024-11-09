06240278516c3d82f0def7c3b477eaaf80fdda48
Abhey Sharma <abhey2526@gmail.com>
2024-11-09 16:50:36 -0500

    Task 13 : Created Server This commit adds server in my_node.cpp. This server a boolean input and updates the publisher when input is true.

--- src/beginner_tutorials/include/beginner_tutorials/my_node.hpp
+++ src/beginner_tutorials/include/beginner_tutorials/my_node.hpp
@@ -2,9 +2,11 @@
 
 #include <example_interfaces/msg/detail/string__struct.hpp>
 #include <example_interfaces/msg/string.hpp>
+#include <example_interfaces/srv/detail/set_bool__struct.hpp>
 #include <rclcpp/node.hpp>
 #include <rclcpp/publisher.hpp>
 #include <rclcpp/rclcpp.hpp>
+#include <rclcpp/service.hpp>
 #include <rclcpp/timer.hpp>
 
 // Declaring MyNode class
@@ -16,8 +18,12 @@ class MyNode : public rclcpp::Node {
  private:
   // Callback function for publishing custom string
   void publishCallback();
+  void serverCallback(const example_interfaces::srv::SetBool::Request::SharedPtr request, 
+                      const example_interfaces::srv::SetBool::Response::SharedPtr response);
 
   // Declaring attributes
   rclcpp::Publisher<example_interfaces::msg::String>::SharedPtr publisher_;
   rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Service<example_interfaces::srv::SetBool>::SharedPtr server_;
+  std::string base_message_;
 };
--- src/beginner_tutorials/src/my_node.cpp
+++ src/beginner_tutorials/src/my_node.cpp
@@ -1,37 +1,65 @@
 #include "beginner_tutorials/my_node.hpp"
 
 #include <chrono>
+#include <example_interfaces/srv/detail/set_bool__struct.hpp>
+#include <functional>
 #include <memory>
 
 #include <example_interfaces/msg/detail/string__struct.hpp>
 #include <rclcpp/logging.hpp>
 #include <rclcpp/utilities.hpp>
+#include <example_interfaces/srv/set_bool.hpp>
 
-
+using std::placeholders::_1;
+using std::placeholders::_2;
 
 // Constructor of MyNode class
-MyNode::MyNode() : Node("my_node") {
+MyNode::MyNode() : Node("my_node"),base_message_{"Hi, My name is Abhey"}
+{
   // Creating publisher
-  publisher_ =
-      this->create_publisher<example_interfaces::msg::String>("my_topic", 10);
+  publisher_ =this->create_publisher<example_interfaces::msg::String>("my_topic", 10);
 
   // Creating timer which calls publishCallback function every 1s
   timer_ = this->create_wall_timer(std::chrono::seconds(1),
                                    std::bind(&MyNode::publishCallback, this));
 
-  RCLCPP_INFO(this->get_logger(), "Publisher Created");
+  // Creating server
+  server_ = this->create_service<example_interfaces::srv::SetBool>("update_publisher", 
+                                                                    std::bind(&MyNode::serverCallback,this,_1,_2));
+
+  RCLCPP_INFO(this->get_logger(), "Server and Publisher Created");
 }
 
 void MyNode::publishCallback() {
   // Creating string object
   auto msg = example_interfaces::msg::String();
-  msg.data = "Hi, My name is Abhey";
+  msg.data = base_message_;
 
   // Printing the msg that is published on "my_topic"
+  // RCLCPP_DEBUG_STREAM(this->get_logger(), "Publishing: " << msg.data);
   RCLCPP_INFO(this->get_logger(), "%s", msg.data.c_str());
 
   // publish
   publisher_->publish(msg);
+}
+
+void MyNode::serverCallback(const example_interfaces::srv::SetBool::Request::SharedPtr request, 
+                            const example_interfaces::srv::SetBool::Response::SharedPtr response)
+{
+  if(request->data)
+  {
+    base_message_ = "This msg has been updated!";
+    RCLCPP_INFO_STREAM(this->get_logger(), "Base message changed successfully.");
+    response->success = true;
+  }
+  else
+  {
+    RCLCPP_WARN(this->get_logger(), "Received request to revert message.");
+    base_message_ = "Hi, My name is Abhey";
+    response->success = false;
+  }
+
+  RCLCPP_DEBUG(this->get_logger(), "Service request processed.");
 }
 
 int main(int argc, char** argv) {
